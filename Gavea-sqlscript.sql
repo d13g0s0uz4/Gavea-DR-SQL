2204ac36c9f664534540d50c9bcd9a9d33426123
suporte-gavea <42077808+suporte-gavea@users.noreply.github.com>
2020-02-06 17:51:36 -0300

    disable script

--- Gavea-sqlscript.sql
+++ Gavea-sqlscript.sql
@@ -1,10 +1,12 @@
 ﻿USE master;
 GO
 
-ALTER DATABASE tempdb 
-MODIFY FILE (NAME = tempdev, FILENAME = 'F:\TEMPDB\tempdb.mdf');
-GO
+--ALTER DATABASE tempdb 
+--MODIFY FILE (NAME = tempdev, FILENAME = 'F:\TEMPDB\tempdb.mdf');
+--GO
 
-ALTER DATABASE tempdb 
-MODIFY FILE (NAME = templog, FILENAME = 'F:\TEMPDB\templog.ldf');
+--ALTER DATABASE tempdb 
+--MODIFY FILE (NAME = templog, FILENAME = 'F:\TEMPDB\templog.ldf');
+--GO
+select @@version
 GO
